439d6071946ce4272dbbccf9691f5fc3f099d0e0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-21 14:58:42 -0300

    FEAT(RespostaMain): Criando classe RespostaMain

---
+++ threads/src/main/java/br.com.alura.threads/RespostaMain.java
@@ -0,0 +1,13 @@
+package br.com.alura.threads;
+
+public class RespostaMain {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("resposta");
+            }
+        }).start();
+    }
+}
