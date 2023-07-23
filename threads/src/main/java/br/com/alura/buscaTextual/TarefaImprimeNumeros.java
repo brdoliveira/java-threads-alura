d22c2638ef00ab3764fb15f89063bce76a987e40
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-23 11:52:33 -0300

    FEAT(TarefaImprimeNumeros): Thread que imprime os numeros

---
+++ threads/src/main/java/br/com/alura/buscaTextual/TarefaImprimeNumeros.java
@@ -0,0 +1,12 @@
+package br.com.alura.buscaTextual;
+
+public class TarefaImprimeNumeros implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i=0;i<1000;i++) {
+            Thread threadAtual = Thread.currentThread();
+            System.out.println(threadAtual.getId() + " - " + i);
+        }
+    }
+}
