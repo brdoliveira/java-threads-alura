76e4d12a2e79b3bb125bda75741cb5ae6840acb4
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-31 19:29:16 -0300

    REFACT(DistribuirTarefas): Alterando Thread

---
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
@@ -0,0 +1,33 @@
+package br.com.alura.servidorTarefas.servidor;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DistribuirTarefas implements Runnable {
+
+    private Socket socket;
+
+    public DistribuirTarefas(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            System.out.println("Distribuindo as tarefas para o cliente " +  socket);
+
+            Scanner entradaCliente = new Scanner(socket.getInputStream());
+
+            while (entradaCliente.hasNextLine()){
+                String comando = entradaCliente.nextLine();
+                System.out.println(comando);
+            }
+
+            entradaCliente.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
