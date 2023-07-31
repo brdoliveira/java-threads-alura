5aeb8225bd75e6d55e191c0240867a4ae8676eea
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-30 23:33:12 -0300

    FEAT(ServidorTarefas): Criando classe ServidorTarefas

---
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/ServidorTarefas.java
@@ -0,0 +1,22 @@
+package br.com.alura.servidorTarefas.servidor;
+
+import br.com.alura.servidorTarefas.DistribuirTarefas;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("---- Iniciando Servidor ----");
+        ServerSocket servidor = new ServerSocket(12345);
+
+        while (true) {
+            Socket socket = servidor.accept();
+            System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+            new Thread(distribuirTarefas).start();
+        }
+    }
+}
