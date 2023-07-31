e00c1af42d423ab664b67201389bb9e53fc9e008
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-31 19:29:28 -0300

    REFACT(ServidorTarefas): Alterando Thread

--- threads/src/main/java/br/com/alura/servidorTarefas/servidor/ServidorTarefas.java
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/ServidorTarefas.java
@@ -1,22 +1,23 @@
 package br.com.alura.servidorTarefas.servidor;
 
-import br.com.alura.servidorTarefas.DistribuirTarefas;
-
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
 
     public static void main(String[] args) throws Exception {
-
         System.out.println("---- Iniciando Servidor ----");
         ServerSocket servidor = new ServerSocket(12345);
+        ExecutorService theadPool = Executors.newCachedThreadPool();
 
         while (true) {
             Socket socket = servidor.accept();
             System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+
             DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-            new Thread(distribuirTarefas).start();
+            theadPool.execute(distribuirTarefas);
         }
     }
 }
