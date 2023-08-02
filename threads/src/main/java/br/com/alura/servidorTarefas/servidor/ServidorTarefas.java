c21c0b417037fa902efc92e533870f3e51e4cc97
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-01 23:59:22 -0300

    REFATORANDO(ServidorTarefas): Alterando classe

--- threads/src/main/java/br/com/alura/servidorTarefas/servidor/ServidorTarefas.java
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/ServidorTarefas.java
@@ -10,14 +10,16 @@ public class ServidorTarefas {
     public static void main(String[] args) throws Exception {
         System.out.println("---- Iniciando Servidor ----");
         ServerSocket servidor = new ServerSocket(12345);
-        ExecutorService theadPool = Executors.newCachedThreadPool();
+        ExecutorService threadPool = Executors.newCachedThreadPool();
 
         while (true) {
             Socket socket = servidor.accept();
-            System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+            System.out.println("Aceitando novo cliente na porta "
+                    + socket.getPort());
 
             DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-            theadPool.execute(distribuirTarefas);
+            threadPool.execute(distribuirTarefas);
         }
     }
+
 }
