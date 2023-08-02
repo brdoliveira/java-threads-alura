0797000dcc3234acd0c49d60770cc08ab5a22539
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-01 23:59:13 -0300

    REFATORANDO(DistribuirTarefas): Alterando classe

--- threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.alura.servidorTarefas.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -15,18 +16,36 @@ public class DistribuirTarefas implements Runnable {
     public void run() {
 
         try {
-            System.out.println("Distribuindo as tarefas para o cliente " +  socket);
+            System.out.println("Distribuindo as tarefas para o cliente " + socket);
 
             Scanner entradaCliente = new Scanner(socket.getInputStream());
 
-            while (entradaCliente.hasNextLine()){
+            PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+
+            while (entradaCliente.hasNextLine()) {
                 String comando = entradaCliente.nextLine();
+                System.out.println("Comando recebido " + comando);
+                switch (comando) {
+                    case "c1": {
+                        // confirmação do o cliente
+                        saidaCliente.println("Confirmação do comando c1");
+                        break;
+                    }
+                    case "c2": {
+                        saidaCliente.println("Confirmação do comando c2");
+                        break;
+                    }
+                    default: {
+                        saidaCliente.println("Comando não encontrado");
+                    }
+                }
                 System.out.println(comando);
             }
 
+            saidaCliente.close();
             entradaCliente.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
