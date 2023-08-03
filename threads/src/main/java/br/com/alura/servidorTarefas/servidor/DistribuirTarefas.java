5d58ee8dbcdfcb998303aecef38e395ccecbba4f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-02 23:55:12 -0300

    REFACT(DistribuirTarefas): Adicionando construtor

--- threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
 public class DistribuirTarefas implements Runnable {
 
     private Socket socket;
+    private ServidorTarefas servidor;
 
-    public DistribuirTarefas(Socket socket) {
+    public DistribuirTarefas(Socket socket, ServidorTarefas servidor) {
         this.socket = socket;
+        this.servidor = servidor;
     }
 
     @Override
     public void run() {
 
         try {
+
             System.out.println("Distribuindo as tarefas para o cliente " + socket);
 
             Scanner entradaCliente = new Scanner(socket.getInputStream());
@@ -23,8 +26,10 @@ public class DistribuirTarefas implements Runnable {
             PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
 
             while (entradaCliente.hasNextLine()) {
+
                 String comando = entradaCliente.nextLine();
                 System.out.println("Comando recebido " + comando);
+
                 switch (comando) {
                     case "c1": {
                         // confirmação do o cliente
@@ -35,18 +40,26 @@ public class DistribuirTarefas implements Runnable {
                         saidaCliente.println("Confirmação do comando c2");
                         break;
                     }
+                    case "fim": {
+                        saidaCliente.println("Desligando o servidor");
+                        servidor.parar();
+                        return;
+                    }
                     default: {
                         saidaCliente.println("Comando não encontrado");
                     }
                 }
+
                 System.out.println(comando);
             }
 
             saidaCliente.close();
             entradaCliente.close();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
     }
 
 }
