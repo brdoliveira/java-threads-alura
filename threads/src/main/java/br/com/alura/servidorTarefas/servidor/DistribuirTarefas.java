2f4918cbb3b8a3f0bba6515313750731fd93e06c
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-03 23:58:29 -0300

    REFACT(DistribuirTarefas): Adicionando Thread

--- threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
+++ threads/src/main/java/br/com/alura/servidorTarefas/servidor/DistribuirTarefas.java
@@ -3,13 +3,16 @@ package br.com.alura.servidorTarefas.servidor;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
 
 public class DistribuirTarefas implements Runnable {
 
     private Socket socket;
     private ServidorTarefas servidor;
+    private ExecutorService threadPool;
 
-    public DistribuirTarefas(Socket socket, ServidorTarefas servidor) {
+    public DistribuirTarefas(ExecutorService threadPool, Socket socket, ServidorTarefas servidor) {
+        this.threadPool = threadPool;
         this.socket = socket;
         this.servidor = servidor;
     }
@@ -32,12 +35,15 @@ public class DistribuirTarefas implements Runnable {
 
                 switch (comando) {
                     case "c1": {
-                        // confirmação do o cliente
                         saidaCliente.println("Confirmação do comando c1");
+                        ComandoC1 c1 = new ComandoC1(saidaCliente);
+                        this.threadPool.execute(c1);
                         break;
                     }
                     case "c2": {
                         saidaCliente.println("Confirmação do comando c2");
+                        ComandoC2 c2 = new ComandoC2(saidaCliente);
+                        this.threadPool.execute(c2);
                         break;
                     }
                     case "fim": {
@@ -50,7 +56,6 @@ public class DistribuirTarefas implements Runnable {
                     }
                 }
 
-                System.out.println(comando);
             }
 
             saidaCliente.close();
